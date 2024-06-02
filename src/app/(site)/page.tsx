8e17ef314b114d7dc01e0235170637da799b9593
kadima <williamsvgw@gmail.com>
2024-06-02 15:58:58 +0100

    implement components and landing page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -4,13 +4,14 @@ import Image from 'next/image'
 import React from 'react'
 import Banner from "../../../public/appBanner.png"
 import Calender from "../../../public/cal.png"
-import { CLIENTS, USERS } from '@/lib/constants'
-import { randomUUID } from 'crypto'
+import Diamond from "../../../public/icons/diamond.svg"
+import CheckIcon from "../../../public/icons/check.svg"
+import { CLIENTS, PRICING_CARDS, PRICING_PLANS, USERS } from '@/lib/constants'
 import { twMerge } from 'tailwind-merge'
 import clsx from 'clsx'
 import CustomCard from '@/components/landing-page/custom-card'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { CardDescription, CardTitle } from '@/components/ui/card'
+import { CardContent, CardDescription, CardTitle } from '@/components/ui/card'
 
 const HomePage = () => {
 	return (
@@ -36,10 +37,10 @@ const HomePage = () => {
 				after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:top-0 after:w-20  after:z-10 after:absolute
 				before:content[""] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r 
 				before:left-0 before:top-0 before:bottom-0 before:w-20 before:z-10 before:absolute'>
-					{[...Array(2)].map((arr) => {
-						return <div key={arr} className='flex flex-nowrap animate-slide'>
-							{CLIENTS.map((client) => {
-								return <div key={client.alt} className='relative w-[200px] m-20 shrink-0 flex items-center'>
+					{[...Array(2)].map((arr,i) => {
+						return <div key={i} className='flex flex-nowrap animate-slide'>
+							{CLIENTS.map((client,i) => {
+								return <div key={i} className='relative w-[200px] m-20 shrink-0 flex items-center'>
 									<Image src={client.logo} alt={client.alt} width={200} className='object-contain max-w-none' />
 								</div>
 							})}
@@ -70,7 +71,7 @@ const HomePage = () => {
 					pill="Testimonials"	/>
 
 					{[...Array(2)].map((arr,index)=>(
-						<div key={randomUUID()} className={twMerge(clsx('mt-10 flex flex-nowrap gap-6 self-start', {
+						<div key={index} className={twMerge(clsx('mt-10 flex flex-nowrap gap-6 self-start', {
 							'flex-row-reverse':index === 1,
 							'animate-[slide_250s_linear_infinite]':true,
 							'animate-[slide_250s_linear_infinite_reverse]':index === 1,
@@ -102,6 +103,63 @@ const HomePage = () => {
 					))}
 				</div>
 							
+			</section>
+
+			<section className='mt-20 px-4 sm:px-6'>
+				<TitleSection
+					title="The Perfect Plan for you"
+					subheading="Experience all the benefit of our platform. Select a plan that suits your needs and take your poductivity to new heights"
+					pill="Pricing"	/>
+
+					<div className='flex flex-col-reverse sm:flex-row gap-4 justify-center sm:items-stretch items-center mt-10'>
+						{PRICING_CARDS.map((card,i)=>(
+							<CustomCard key={i} className={clsx("w-[300px] rounded-2xl dark:bg-black/40 background-blur-3xl relative",
+							{'border-brand-primaryPurple/70': card.planType === PRICING_PLANS.proplan}
+							)} 
+							cardHeader={
+								<CardTitle className='text-2xl font-semibold'>
+									{card.planType === PRICING_PLANS.proplan && 
+										(<>
+											<div className='hidden dark:block w-full blur-[120px] rounded-full h-32 absolute 
+											bg-brand-primaryPurple/80 -z-10 top-0'/>
+											<Image src={Diamond} alt='Pro plan' className='absolute top-6 right-6'/>
+										 </>
+										)
+									}
+
+									{card.planType}
+								</CardTitle>
+								}
+
+								cardContent={
+									<CardContent className='p-0'>
+										<span className='font-normal text-2xl'>${card.price}</span>
+										{+card.price > 0 ? 
+										<span className='dark:text-washed-purple-800 ml-1'>/mo</span> :''}
+										<p className='dark:text-washed-purple-800'>{card.description}</p>
+										<Button variant={'btn-primary'} className='whitespace-nowrap w-full nt-4'>
+											{card.planType === PRICING_PLANS.proplan ? 'Go Pro' : 'Get Started'}
+										</Button>
+									</CardContent>
+								}
+								
+								cardFooter={
+									<ul className='font-normal flex mb-2 flex-col gap-4'>
+										<small>{card.highlightFeature}</small>
+										{card.freatures.map((feature,index)=>{
+											return <li key={index}
+											className='flex items-center gap-2'>
+											<Image src={CheckIcon} alt='Check Icon'/>
+											<p>{feature}</p>	
+										</li>
+										})}
+										
+									</ul> 
+								}>
+
+							</CustomCard>
+						))}
+					</div>
 			</section>
 		</>
 
